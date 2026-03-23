fe6835e86b61ff9121ef403b6f7aa64fe663a99a
nina <your@email.com>
2026-03-23 13:27:13 +0700

    fix path issue

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const express = require("express");
+const path = require("path");
 const app = express();
 
-// يخلي السيرفر يقرأ ملفات HTML من public
-app.use(express.static("public"));
+// يخلي السيرفر يقرأ ملفات public
+app.use(express.static(path.join(__dirname, "public")));
 
 // الصفحة الرئيسية
 app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/public/register.html");
+  res.sendFile(path.join(__dirname, "public", "register.html"));
 });
 
 const PORT = process.env.PORT || 3000;
