46e8fb8283cbf21edc3ab886765fa1ee255e0a84
nina <your@email.com>
2026-03-23 14:15:30 +0700

    fix port

--- app.js
+++ app.js
@@ -749,10 +749,11 @@ alert("Added to cart");
 </html>`);
 });
 // ================= START SERVER =================
-app.listen(3000, () => {
-    console.log("Server started on http://localhost:3000");
-});
+const PORT = process.env.PORT || 3000;
 
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
 // ================= WALLET PAGE =================
 app.get("/wallet", (req, res) => {
 res.send(`<!DOCTYPE html>
