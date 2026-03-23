52b8a3afb5018658521b7b807a70ae5ec8ffc4ab
nina <your@email.com>
2026-03-23 13:21:51 +0700

    fix home route

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const app = express();
 
-// يخلي السيرفر يقرأ ملفات HTML
+// يخلي السيرفر يقرأ ملفات HTML من public
 app.use(express.static("public"));
+
+// الصفحة الرئيسية
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/public/register.html");
+});
 
 const PORT = process.env.PORT || 3000;
 
